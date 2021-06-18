840bc8141085ab3f435065576dae8cfc1a2edccb
WahyuKris <84218401+wahyuwijayakristianto@users.noreply.github.com>
2021-06-19 00:41:38 +0700

    Add files via upload
    project seleksi ASDOS

---
+++ tesasdos/src/tesasdos/Tesasdos.java
@@ -0,0 +1,99 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tesasdos;
+
+/**
+ *
+ * @author User
+ */
+public class Tesasdos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        for (int a = 0; a < 6; a++) {
+            System.out.print(" ");
+        }
+        for (int e = 0; e < 1; e++) {
+            System.out.print("*");
+            System.out.println("");
+        }
+        for (int a = 0; a < 5; a++) {
+            System.out.print(" ");
+        }
+
+        for (int e = 0; e < 1; e++) {
+            for (int g = 0; g <= 2; g++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+        for (int a = 0; a < 4; a++) {
+            System.out.print(" ");
+        }
+
+        for (int e = 0; e < 1; e++) {
+            for (int g = 0; g <= 1; g++) {
+                System.out.print("*");
+            }
+            for (int h = 0; h <1;h++){
+                System.out.print(" ");}
+            for (int g = 0; g <= 1; g++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+        
+        for (int a = 0; a <3; a++) {
+            System.out.print(" ");
+        }
+
+        for (int e = 0; e < 1; e++) {
+            for (int g = 0; g <= 1; g++) {
+                System.out.print("*");
+            }for (int g = 0; g < 1; g++) {
+                System.out.print(" ");
+            }
+            for (int h = 0; h <1;h++){
+                System.out.print("*");}
+            for (int g = 0; g < 1; g++) {
+                System.out.print(" ");
+            }
+            for (int g = 0; g <= 1; g++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+        
+        for (int a = 0; a <2; a++) {
+            System.out.print(" ");
+        }
+
+        for (int e = 0; e < 1; e++) {
+            for (int g = 0; g <= 1; g++) {
+                System.out.print("*");
+            }
+            for (int h = 0; h <5;h++){
+                System.out.print(" ");}
+            for (int g = 0; g <= 1; g++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }for (int a = 0; a <1; a++) {
+            System.out.print(" ");
+        }
+        for (int e = 0; e < 1; e++) {
+            for (int g = 1; g <= 11; g++) {
+                System.out.print("*");
+            }System.out.println("");}
+        for (int e = 0; e < 1; e++) {
+            for (int g = 1; g <= 13; g++) {
+                System.out.print("*");
+            }}
+    }
+}
